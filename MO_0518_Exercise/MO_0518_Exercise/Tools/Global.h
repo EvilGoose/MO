f746b0b6ddedbefcde95ee94256e092f9313a4b9
EvilGoose <fangweiyi945@163.com>
2016-05-19 12:02:55 +0800

    小修小补

--- MO_0518_Exercise/MO_0518_Exercise/Tools/Global.h
+++ MO_0518_Exercise/MO_0518_Exercise/Tools/Global.h
@@ -9,6 +9,12 @@
 #ifndef Global_h
 #define Global_h
 
+
+///运行时判断运行版本
+
+//判断当前设备
+
+
 //屏幕的长, 宽
 #define kScreenWidth [[UIScreen mainScreen] bounds].size.width
 #define kScreenHeight [[UIScreen mainScreen] bounds].size.height
@@ -20,7 +26,17 @@
 
 //自定义log
 
+
+//自定义Log,且可以打印行号
+#ifdef DEBUG
+#define NSLog(FORMAT, ...) fprintf(stderr,"%s:%d\t%s\n",[[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
+#else
+#define NSLog(FORMAT, ...) nil
+#endif
+
 //转颜色
 
 
 #endif /* Global_h */
+
+
--- MO_0518_Exercise/MO_0518_Exercise/UserViewController/UserDetailViewController/EGUserDetailViewController.m
+++ MO_0518_Exercise/MO_0518_Exercise/UserViewController/UserDetailViewController/EGUserDetailViewController.m
@@ -14,9 +14,34 @@
 
 @implementation EGUserDetailViewController
 
+#pragma mark - life cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+}
+
+//当前界面出现前修改navigationBar的状态
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self setNaviagationBarTranspatent];
+}
+
+//当前界面消失前恢复到navigationBar的初始状态
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self setNavigationBarOrigin];
+}
+
+#pragma mark - set NavigationBar status
+
+//设置Navigationbar 状态
+- (void)setNaviagationBarTranspatent {
+    NSLog(@"%@", @"aaa");
+}
+
+- (void)setNavigationBarOrigin  {
+    NSLog(@"%s", __func__);
 }
 
 @end
